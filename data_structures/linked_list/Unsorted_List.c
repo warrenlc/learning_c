d2417e863ae2d4a193e1514f4094aacc2a1f6c05
warrenlc <warrencrutcher@gmail.com>
2023-03-07 14:17:01 +0100

    done with unosrted Int. moving to double

--- data_structures/linked_list/Unsorted_List.c
+++ data_structures/linked_list/Unsorted_List.c
@@ -4,134 +4,6 @@
 #include "Unsorted_List.h"
 
 int main() {
-/*
-    Unsorted_List_int *int_list = Unsorted_List_int_create();
-    if (Unsorted_List_int_is_empty(&int_list) == 1) {
-        printf("head is Null\n");
-    }
-    
-    Unsorted_List_int_add_front(&int_list, 11);
-
-    if (Unsorted_List_int_is_empty(&int_list) == 1) {
-        printf("head is Null\n");
-    } else {
-        printf("Head is not null, but has value: %d\n", int_list->head->value); 
-    }
-    
-    unsigned k;
-    for(k = 4; k < 1000; k++) {
-        if (k % 19 == 0) {
-            Unsorted_List_int_add_front(&int_list, (int)k);
-        }
-    }
-    
-    Unsorted_List_int_add_back(&int_list, 7);
-    for (k = 19; k < 77; k++) {
-        if (k % 2 != 0) {
-            Unsorted_List_int_add_back(&int_list, (int)k);
-        }
-    } 
-    
-    printf("\nThe size of the list is: %lu\n", Unsorted_List_int_length(int_list));
-    Unsorted_List_int_print(int_list);
-
-    printf("The value of the list at position %u is %d\n", k, *Unsorted_List_int_value_at(int_list, (size_t)k));
-    for (k = 1; k < 29; k++) {
-        Unsorted_List_int_remove_head(&int_list);
-    }
-    
-
-    printf("\nThe size of the list is: %lu\n", Unsorted_List_int_length(int_list));
-    Unsorted_List_int_print(int_list);
-    
-    if (Unsorted_List_int_insert_at(&int_list, 0, 973) == 1) {
-        printf("\nadded.\n\n");
-    } else {
-        printf("\nOut of range.\n\n");
-    }
-    
-    Unsorted_List_int_print(int_list);
-
-
-    if (Unsorted_List_int_insert_at(&int_list, 234234, 12123) == 1) {
-        printf("\nadded.\n\n");
-    } else {
-        printf("\nOut of range.\n\n");
-    }
-    
-    if (Unsorted_List_int_insert_at(&int_list, 10, 713) == 1) {
-        printf("\nadded.\n\n");
-    } else {
-        printf("\nOut of range.\n\n");
-    } 
-    printf("\n");
-    Unsorted_List_int_print(int_list);    
-    
-    printf("\nThe size of the list is: %lu\n", Unsorted_List_int_length(int_list));
-    if (Unsorted_List_int_remove_at(&int_list, 57) == 1) {
-        printf("removed!\n\n");
-    } else {
-        printf("cannot remove. out of range!\n\n");
-    }
-    Unsorted_List_int_print(int_list);
-    Unsorted_List_int_destroy(&int_list);
-*/
-
-    Unsorted_List_int* fuck = Unsorted_List_int_create();
-
-    Unsorted_List_int_print(fuck);
-    printf("\n");
-    
-    Unsorted_List_int_insert_at(&fuck, 0, 13);
-    Unsorted_List_int_print(fuck);
-    printf("\n");
-
-    Unsorted_List_int_add_back(&fuck, 17);
-    Unsorted_List_int_print(fuck);
-    printf("\n");
-    
-    Unsorted_List_int_insert_at(&fuck, 1, 123);
-    Unsorted_List_int_print(fuck);
-    printf("\n");
-    
-    Unsorted_List_int_insert_at(&fuck, Unsorted_List_int_length(fuck), 19);
-    Unsorted_List_int_print(fuck);
-    printf("\n");
-
-    Unsorted_List_int_insert_at(&fuck, 0, 13);
-    Unsorted_List_int_print(fuck);
-    printf("\n");
-    
-    Unsorted_List_int_insert_at(&fuck, 1, 17);
-    Unsorted_List_int_print(fuck);
-    printf("\n");
-    
-    Unsorted_List_int_insert_at(&fuck, Unsorted_List_int_length(fuck), 19);
-    Unsorted_List_int_print(fuck);
-    printf("\n");
-
-    Unsorted_List_int_add_front(&fuck, 999);
-    Unsorted_List_int_print(fuck);
-    printf("\n");
-
-    Unsorted_List_int_insert_at(&fuck, 0, 777);
-    Unsorted_List_int_print(fuck);
-    printf("\n");
-
-    Unsorted_List_int_insert_at(&fuck, Unsorted_List_int_length(fuck) - 1, 666);
-    Unsorted_List_int_print(fuck);
-    printf("\n");
-   
-
-    Unsorted_List_int_add_back(&fuck, 888);
-    Unsorted_List_int_print(fuck);
-    printf("\n");
-    
-    Unsorted_List_int_remove_at(&fuck, Unsorted_List_int_length(fuck));
-    Unsorted_List_int_print(fuck);
-    printf("\n");
-
-    Unsorted_List_int_destroy(&fuck);
     return 0;
 }
 
@@ -142,15 +14,21 @@ Unsorted_List_int_create() {
     return new_list;
 }
 
+/****************************************************************************************
+ * 
+ * Singly linked unsorted List of type int
+ *
+ ***************************************************************************************/
+
 int
 Unsorted_List_int_is_empty(Unsorted_List_int **list) {
-    return (*list)->head == NULL;
+    return !(*list)->head;
 }
 
 void 
 Unsorted_List_int_destroy(Unsorted_List_int **list) {
     Node_int *temp;
-    while ((*list)->head != NULL) {
+    while ((*list)->head) {
         temp = (*list)->head;
         (*list)->head = (*list)->head->next;
         free(temp);
@@ -171,11 +49,12 @@ Unsorted_List_int_add_back(Unsorted_List_int **list, int val) {
     Node_int *new_node = malloc(sizeof(Node_int));
     new_node->value = val;
     new_node->next = NULL;
-    if ((*list)->head == NULL) { 
+    
+    if (!(*list)->head) { 
         (*list)->head = new_node; 
     } else {
         Node_int *current = (*list)->head;
-        while (current->next != NULL) {
+        while (current->next) {
             current = current->next;
         }
         current->next = new_node;
@@ -187,8 +66,8 @@ Unsorted_List_int_print(Unsorted_List_int *list) {
     const Node_int *temp;
     printf("{");
     temp = (list)->head;
-    while (temp != NULL) {
-        if (temp->next == NULL) {
+    while (temp) {
+        if (!temp->next) {
             printf("%d", temp->value);
         }
         else {
@@ -201,7 +80,7 @@ Unsorted_List_int_print(Unsorted_List_int *list) {
 
 void 
 Unsorted_List_int_remove_head(Unsorted_List_int **list) {
-    if (Unsorted_List_int_is_empty(list) != 1) { 
+    if (!Unsorted_List_int_is_empty(list)) { 
         Node_int *temp_head = (*list)->head; 
         (*list)->head  = (*list)->head->next;
         free(temp_head); 
@@ -210,34 +89,47 @@ Unsorted_List_int_remove_head(Unsorted_List_int **list) {
 
 size_t 
 Unsorted_List_int_length(const Unsorted_List_int *list) {
-    if (list->head == NULL) return 0; 
+    if (!list->head) 
+        return 0; 
+    
     size_t k = 0;
     const Node_int *temp = list->head;
-    while (temp != NULL) {
+    while (temp) {
         temp = temp->next;
         k++;
     }
+    
     return k; 
 }
 
 const int* 
 Unsorted_List_int_value_at(const Unsorted_List_int *list, size_t index) {
-    if (list->head == NULL) return NULL; 
+    if (!list->head) 
+        return NULL; 
+    
     const Node_int *current = list->head;
     for (size_t i = 0; i < index; i++) {
         current = current->next;
-        if (current == NULL) return NULL;
+        if (!current) 
+            return NULL;
     }
+    
     return &(current->value);
 }
 
 int 
 Unsorted_List_int_insert_at(Unsorted_List_int **list, size_t index, int val) {
-    if (index == 0) { Unsorted_List_int_add_front(list, val); return 1; }
-    if (Unsorted_List_int_is_empty(list) == 1) { return -1; }
+    if (index == 0) {
+        Unsorted_List_int_add_front(list, val); 
+        return 1; 
+    }
+    
+    // We can only add to an empty list at index 0, which is handled above.
+    if (Unsorted_List_int_is_empty(list)) 
+        return -1; 
+    
     Node_int *temp = (*list)->head;
-    size_t k;
-    for (k = 0; k < index - 1; k++) {
+    for (size_t k = 0; k < index - 1; k++) {
 
         if (temp->next == NULL) { return -1; } 
         temp = temp->next;
@@ -247,27 +139,57 @@ Unsorted_List_int_insert_at(Unsorted_List_int **list, size_t index, int val) {
     new_node->value = val;
     new_node->next = temp->next;
     temp->next = new_node;
+
     return 1;
 }
 
 int 
 Unsorted_List_int_remove_at(Unsorted_List_int **list, size_t index) {
-    if (Unsorted_List_int_is_empty(list) == 1) { return -1; }
-    if (index == 0) { Unsorted_List_int_remove_head(list); return 1; }
-    Node_int *temp = (*list)->head->next;
-    size_t k;
-    for (k = 1; k < index - 2; k++) {
-        if (temp->next == NULL) { printf("outof fucking boundS!\n"); return -1; }
-        temp = temp->next;
+    if (Unsorted_List_int_is_empty(list)) 
+        return -1;
+   
+    if (index == 0) { 
+        Unsorted_List_int_remove_head(list); 
+        return 1; 
     }
-    Node_int *n = temp->next->next;
-    free(temp->next);
-    temp->next = n;
-    return 1;
+    
+    Node_int *temp = (*list)->head; 
+    size_t k = 0;
+    while (temp->next) {
+        if (k == index - 1) {
+            Node_int *n = temp->next->next;
+            free(temp->next);
+            temp->next = n;
+            return 1;
+        }
+        temp = temp->next;
+        k++;
+    }
+    return -1;
 }
 
-//void Unsorted_List_int_remove_back(Unsorted_List_int **list);
+void 
+Unsorted_List_int_remove_back(Unsorted_List_int **list) {
+    if (Unsorted_List_int_is_empty(list))
+        return; 
 
+    Node_int *counter = (*list)->head;
+    while (counter->next->next) {
+        counter = counter->next;        
+    }
+
+    free(counter->next);
+    counter->next = NULL; 
+}
+
+/**********************************************************************************************
+ *
+ * Singly linked unsorted list of type double
+ *
+ *********************************************************************************************/
+
+Unsorted_List_double 
+*Unsorted_List_double_create() {}
 
 
 
--- data_structures/linked_list/Unsorted_List.h
+++ data_structures/linked_list/Unsorted_List.h
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 
-/*
+/****************************************************************
  * Singly linked lists
- * */
+ ***************************************************************/
 
-/*
+/***************************
  * Integer Lists
- * */
+ *******************'******/
 struct Node_int {
     int value;
     struct Node_int *next;
@@ -18,9 +18,9 @@ typedef struct {
 }Unsorted_List_int;
 
 
-/*
+/***************************
  * Double Lists
- * */
+ **************************/
 struct Node_double {
     double value;
     struct Node_double *next;
@@ -32,9 +32,9 @@ typedef struct {
 }Unsorted_List_double;
 
 
-/*
+/***************************
  * String Lists
- * */
+ **************************/
 struct Node_string {
     const char *string;
     struct Node_string *next;
@@ -46,15 +46,15 @@ typedef struct {
 }Unsorted_List_string;
 
 
-/*
+/*************************'**************************************
  * Functions
- * */
+ ***************************************************************/
 
-/*
+/***************************
  * Unsorted integer lists
- * */
-Unsorted_List_int*
-Unsorted_List_int_create();
+ **************************/
+
+Unsorted_List_int *Unsorted_List_int_create();
 int Unsorted_List_int_is_empty(Unsorted_List_int **list);
 void Unsorted_List_int_destroy(Unsorted_List_int **list);
 void Unsorted_List_int_add_front(Unsorted_List_int **list, int val);
@@ -67,6 +67,36 @@ int Unsorted_List_int_insert_at(Unsorted_List_int **list, size_t index, int val)
 int Unsorted_List_int_remove_at(Unsorted_List_int **list, size_t index);
 void Unsorted_List_int_remove_back(Unsorted_List_int **list);
 
+/***************************
+ * Unsorted double lists
+ **************************/
 
+Unsorted_List_double *Unsorted_List_double_create();
+int Unsorted_List_double_is_empty(Unsorted_List_double **list);
+void Unsorted_List_double_destroy(Unsorted_List_double **list);
+void Unsorted_List_double_add_front(Unsorted_List_double **list, double val);
+void Unsorted_List_double_add_back(Unsorted_List_double **list, double val);
+void Unsorted_List_double_print(Unsorted_List_double *list);
+void Unsorted_List_double_remove_head(Unsorted_List_double **list);
+size_t Unsorted_List_double_length(const Unsorted_List_double *list);
+const double* Unsorted_List_double_value_at(const Unsorted_List_double **list, size_t index);
+int Unsorted_List_double_insert_at(Unsorted_List_double **list, size_t index, double val);
+int Unsorted_List_double_remove_at(Unsorted_List_double **list, size_t index);
+void Unsorted_List_double_remove_back(Unsorted_List_double **list);
 
+/**************************
+ * Unsorted string lists
+ *************************/
 
+Unsorted_List_string *Unsorted_List_string_create();
+int Unsorted_List_string_is_empty(Unsorted_List_string **list);
+void Unsorted_List_string_destroy(Unsorted_List_string **list);
+void Unsorted_List_string_add_front(Unsorted_List_string **list, char *str);
+void Unsorted_List_string_add_back(Unsorted_List_string **list, char *str);
+void Unsorted_List_string_print(Unsorted_List_string *list);
+void Unsorted_List_string_remove_head(Unsorted_List_string **list);
+size_t Unsorted_List_string_length(const Unsorted_List_string *list);
+const char* Unsorted_List_string_value_at(const Unsorted_List_string **list, size_t index);
+int Unsorted_List_string_insert_at(Unsorted_List_string **list, size_t index, char *str);
+int Unsorted_List_string_remove_at(Unsorted_List_string **list, size_t index);
+void Unsorted_List_string_remove_back(Unsorted_List_string **list);
