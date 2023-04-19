c26242794aef0a166954f5f3c0a428f706a1513f
warrenlc <88832598+warrenlc@users.noreply.github.com>
2023-04-19 23:21:05 +0200

    Update tree.c

--- data_structures/tree/tree.c
+++ data_structures/tree/tree.c
@@ -6,7 +6,7 @@
 
 int main(int argc, char *argv[]) {
     tree *binary_tree = malloc(sizeof(tree));
-    binary_tree->value = fuck;
+    binary_tree->value = NULL;
     binary_tree->left = NULL;
     binary_tree->right = NULL;
     printf("our initialized tree with value %d and no children.\n", binary_tree->value);
